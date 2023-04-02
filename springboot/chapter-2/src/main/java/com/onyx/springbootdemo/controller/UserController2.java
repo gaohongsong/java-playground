ae1579872b3e743446ef3688f50190d3c108a66d
gaomugong <gaomugong@qq.com>
2023-04-02 19:02:54 +0800

    feat: hello jpa

--- springboot/chapter-2/src/main/java/com/onyx/springbootdemo/controller/UserController2.java
+++ springboot/chapter-2/src/main/java/com/onyx/springbootdemo/controller/UserController2.java
@@ -2,12 +2,15 @@ package com.onyx.springbootdemo.controller;
 
 import com.onyx.springbootdemo.dao.UserRepository;
 import com.onyx.springbootdemo.pojo.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Api(value = "User Service", tags = {"用户模块2"})
 @RestController
 @RequestMapping("/demo")
 public class UserController2 {
@@ -16,6 +19,7 @@ public class UserController2 {
     private UserRepository userRepository;
 
     @PostMapping("/save")
+    @ApiOperation("save")
     public ResponseEntity<String> saveUser(@Valid @RequestBody User user) {
         // https://www.geeksforgeeks.org/spring-boot-validation-using-hibernate-validator/
         userRepository.save(user);
@@ -23,6 +27,7 @@ public class UserController2 {
     }
 
     @PostMapping("/add")
+    @ApiOperation("add")
     public String addNewUser(@RequestParam String username, @RequestParam int age) {
         User u = new User();
         u.setUsername(username);
@@ -34,11 +39,13 @@ public class UserController2 {
     }
 
     @GetMapping("/all")
+    @ApiOperation("all")
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/filter")
+    @ApiOperation("filter")
     public Iterable<User> filterUsers(@RequestParam String username) {
         return userRepository.findByUsername(username);
     }
