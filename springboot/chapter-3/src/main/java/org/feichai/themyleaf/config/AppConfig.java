fe2bdcf23b5e230e3f97779c19b248eee142790c
gaomugong <gaomugong@qq.com>
2023-04-06 01:55:06 +0800

    hello redisTemplate

---
+++ springboot/chapter-3/src/main/java/org/feichai/themyleaf/config/AppConfig.java
@@ -0,0 +1,8 @@
+package org.feichai.themyleaf.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class AppConfig {
+
+}
--- springboot/chapter-3/src/main/java/org/feichai/themyleaf/config/LettuceRedisConfig.java
+++ springboot/chapter-3/src/main/java/org/feichai/themyleaf/config/LettuceRedisConfig.java
@@ -2,6 +2,7 @@ package org.feichai.themyleaf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -11,6 +12,12 @@ import java.io.Serializable;
 
 @Configuration
 public class LettuceRedisConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+        // return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+    }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
