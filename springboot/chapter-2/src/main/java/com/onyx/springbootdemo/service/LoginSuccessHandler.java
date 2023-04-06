ccca9e28adf8f3971e60c52eeff7c347d7b99f73
gaomugong <gaomugong@qq.com>
2023-04-06 15:46:11 +0800

    feat: hello security

--- springboot/chapter-2/src/main/java/com/onyx/springbootdemo/service/LoginSuccessHandler.java
+++ springboot/chapter-2/src/main/java/com/onyx/springbootdemo/service/LoginSuccessHandler.java
@@ -4,14 +4,15 @@ package com.onyx.springbootdemo.service;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException {
         System.out.println("Login success");
         response.sendRedirect("/home");
     }
