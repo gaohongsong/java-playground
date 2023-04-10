a4761fd447091d0ad6e6cce81cd42a756353225e
gaomugong <gaomugong@qq.com>
2023-04-10 17:51:18 +0800

    remove extension

--- springboot/voteman/src/test/java/org/feichai/system/service/impl/UserServiceImplTest.java
+++ springboot/voteman/src/test/java/org/feichai/system/service/impl/UserServiceImplTest.java
@@ -3,14 +3,15 @@ package org.feichai.system.service.impl;
 import org.feichai.system.domain.User;
 import org.feichai.system.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceImplTest {
-    @Autowired
+    @Resource
     private UserService userService;
     @Test
     void findByName() {
